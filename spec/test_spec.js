71401415549a98833e06c6fc29d767bef176ac20
Unknown <Meike.steinleger@t-online.de>
2017-10-07 19:49:26 +0200

    add test with wrong Webtoken

--- spec/test_spec.js
+++ spec/test_spec.js
@@ -6,13 +6,18 @@ var server = require('../tasks/serve.js'),
 const { createFetch, createStack, enableRecv, header, base, parse } = require('http-client');
 
 describe('Server', function(){
-    var options= {
+    var options_right= {
         headers : {
             'webtoken' : 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpYXQiOjE1MDcyODA5OTIsImV4cCI6MTUwNzMxNjk5Mn0.Ogh_OmlziSCYpV16YoNUKhyWDTOfaWZnBfQMahW0pEfrzNrnHybmr0OBHk_YBHdNR0ocOM1bvHD4eH-ClM3JkNpl8bhMaMkmnLtk8Jw9iKX8WH-ecfZ8BQ1OT2ohLYoFHFFnUCfopQEM2v2wEz3iCVGPY4KkQ5lX-wE39D5XEJZyxYlxXM2XLXgEacxtgrbFJeO8ECZ6UcLpjtEr-LGuKVJh5gxdLRKK44fm2sDqtMKR_IMnY6-zBEo5wtr-oJ2JxpzR5UdKR6qZ2xCOaOuvBD36eW-05HK1BWEbEh43WZ7LAGh1MfQU-8clVBEDMvwqr0vgMu1MZ1XnhLoA20bkvA'            
         }
     }   
+    var options_wrong= {
+        headers : {
+            'webtoken' : 'false'            
+        }
+    }  
     describe('GET / return status code', function(){
-        it("401 because unauth", function(done){
+        it("401 because it is an unauthorized request without Webtoken", function(done){
             const fetch = enableRecv(
                 require('node-fetch')
             );
@@ -25,7 +30,7 @@ describe('Server', function(){
                 done();
             });
         });
-        it("200 because auth", function(done){
+        it("401 because it is an unauthorized request with wrong Webtoken", function(done){
             const fetch = enableRecv(
                 require('node-fetch')
             );
@@ -33,14 +38,27 @@ describe('Server', function(){
                 base(base_url),
                 parse('json')
             );
-            fetch(base_url, options).then(function(response){
+            fetch(base_url, options_wrong).then(function(response){
+                expect(response.status).toBe(401);
+                done();
+            });
+        });
+        it("200 because it is an authorized request and the url exists", function(done){
+            const fetch = enableRecv(
+                require('node-fetch')
+            );
+            const stack = createStack(
+                base(base_url),
+                parse('json')
+            );
+            fetch(base_url, options_right).then(function(response){
                 expect(response.status).toBe(200);
                 done();
             });
         }); 
     });
     describe('GET task/_serve_selftest  return status code', function(){
-        it("401 because unauth", function(done){
+        it("401 because it is an unauthorized request without Webtoken", function(done){
             const fetch = enableRecv(
                 require('node-fetch')
             );
@@ -53,7 +71,7 @@ describe('Server', function(){
                 done();
             });
         });
-        it("200 because", function(done){
+        it("401 because it is an unauthorized request with wrong Webtoken", function(done){
             const fetch = enableRecv(
                 require('node-fetch')
             );
@@ -61,14 +79,27 @@ describe('Server', function(){
                 base(task_url),
                 parse('json')
             );
-            fetch(task_url, options).then(function(response){
+            fetch(task_url, options_wrong).then(function(response){
+                expect(response.status).toBe(401);
+                done();
+            });
+        });
+        it("200 because it is an authorized request and the task exists", function(done){
+            const fetch = enableRecv(
+                require('node-fetch')
+            );
+            const stack = createStack(
+                base(task_url),
+                parse('json')
+            );
+            fetch(task_url, options_right).then(function(response){
                 expect(response.status).toBe(200);
                 done();
             });
         });
     });
     describe('GET task/fail_serve_selftest_  return status code', function(){
-        it("401 because unauth", function(done){
+        it("401 because it is an unauthorized request", function(done){
             const fetch = enableRecv(
                 require('node-fetch')
             );
@@ -81,7 +112,7 @@ describe('Server', function(){
                 done();
             });
         });
-        it("500 because", function(done){
+        it("500 because the task do not exist", function(done){
             const fetch = enableRecv(
                 require('node-fetch')
             );
@@ -89,7 +120,7 @@ describe('Server', function(){
                 base(task_url),
                 parse('json')
             );
-            fetch(task_url_fail, options).then(function(response){
+            fetch(task_url_fail, options_right).then(function(response){
                 expect(response.status).toBe(500);
                 done();
             });
